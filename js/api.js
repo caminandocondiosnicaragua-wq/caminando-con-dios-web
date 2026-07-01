fdb32f8a71376d7bfe4445d8a6acfb092bd8dad8
caminandocondiosnicaragua-wq <caminandocondiosnicaragua@gmail.com>
2026-07-01 14:45:12 -0600

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,9 +1,13 @@
-const API_URL="https://script.google.com/macros/s/AKfycbxpCZ00WGFhINXrmOEs2NmVWOXIw_-_mmO0Da_XGfrWzLzuZyki2kzercmR14I7HlkNAw/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbxpCZ00WGFhINXrmOEs2NmVWOXIw_-_mmO0Da_XGfrWzLzuZyki2kzercmR14I7HlkNAw/exec";
 
-async function obtenerDevocionales(){
+async function obtenerDevocionales() {
 
-const respuesta=await fetch(API_URL);
+    const respuesta = await fetch(API_URL);
 
-return await respuesta.json();
+    if (!respuesta.ok) {
+        throw new Error("No fue posible obtener los datos.");
+    }
+
+    return await respuesta.json();
 
 }
