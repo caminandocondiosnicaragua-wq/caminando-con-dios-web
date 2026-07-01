87c7354ca0e4c212845c7a90f2ddffdf1529817a
caminandocondiosnicaragua-wq <caminandocondiosnicaragua@gmail.com>
2026-07-01 16:10:40 -0600

    Update api.js

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,1 +1,17 @@
+const API_URL =
+"https://script.google.com/macros/s/AKfycbxpCZ00WGFhINXrmOEs2NmVWOXIw_-_mmO0Da_XGfrWzLzuZyki2kzercmR14I7HlkNAw/exec";
 
+/************************************************
+ * Obtiene el devocional del día
+ ***********************************************/
+async function obtenerDevocional() {
+
+    const respuesta = await fetch(API_URL);
+
+    if (!respuesta.ok) {
+        throw new Error("No fue posible conectar con la API.");
+    }
+
+    return await respuesta.json();
+
+}
