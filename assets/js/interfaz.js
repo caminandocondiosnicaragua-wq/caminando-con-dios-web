37179499bafad3fc1cbfaeae6be94aeeb191308c
caminandocondiosnicaragua-wq <caminandocondiosnicaragua@gmail.com>
2026-07-01 16:19:25 -0600

    Update interfaz.js

--- assets/js/interfaz.js
+++ assets/js/interfaz.js
@@ -1,1 +1,29 @@
+function mostrarDevocional(devocional){
 
+    const app = document.getElementById("app");
+
+    app.innerHTML = `
+
+        <section class="contenedor">
+
+            <h1>Caminando con Dios</h1>
+
+            <p>${devocional.FECHA}</p>
+
+            <h2>${devocional.TITULO}</h2>
+
+            <h3>${devocional["TEXTO A.T."]}</h3>
+
+            <h3>${devocional["TEXTO N.T."]}</h3>
+
+            <hr>
+
+            <h2>Enseñanza</h2>
+
+            <p>${devocional.ENSEÑANZA}</p>
+
+        </section>
+
+    `;
+
+}
