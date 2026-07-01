a7136477350c39fad41ac73d0a14fa44e7e78df7
caminandocondiosnicaragua-wq <caminandocondiosnicaragua@gmail.com>
2026-07-01 16:11:04 -0600

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,26 @@
+window.onload = iniciar;
 
+async function iniciar(){
+
+    console.clear();
+
+    console.log("================================");
+    console.log("CAMINANDO CON DIOS");
+    console.log("Iniciando plataforma...");
+    console.log("================================");
+
+    try{
+
+        const datos = await obtenerDevocional();
+
+        console.log(datos);
+
+    }
+
+    catch(error){
+
+        console.error(error);
+
+    }
+
+}
