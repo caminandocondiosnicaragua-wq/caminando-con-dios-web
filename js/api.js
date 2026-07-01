af1ae8015b80251d176f3b36a632e12d339b1e26
caminandocondiosnicaragua-wq <caminandocondiosnicaragua@gmail.com>
2026-07-01 14:40:54 -0600

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,1 +1,9 @@
-const API_URL = "AQUI_VA_TU_URL_DE_APPS_SCRIPT";
+const API_URL="https://script.google.com/macros/s/AKfycbxpCZ00WGFhINXrmOEs2NmVWOXIw_-_mmO0Da_XGfrWzLzuZyki2kzercmR14I7HlkNAw/exec";
+
+async function obtenerDevocionales(){
+
+const respuesta=await fetch(API_URL);
+
+return await respuesta.json();
+
+}
