f8434205ebcce47d9024efdafce2c4612e7fd883
caminandocondiosnicaragua-wq <caminandocondiosnicaragua@gmail.com>
2026-07-01 20:01:00 -0600

    Update api.js

--- assets/js/api.js
+++ assets/js/api.js
@@ -2,7 +2,7 @@ const API_URL =
 "https://script.google.com/macros/s/AKfycbxpCZ00WGFhINXrmOEs2NmVWOXIw_-_mmO0Da_XGfrWzLzuZyki2kzercmR14I7HlkNAw/exec";
 
 /************************************************
- * Obtiene el devocional del día
+ * Obtiene el devocional
  ***********************************************/
 async function obtenerDevocional() {
 
