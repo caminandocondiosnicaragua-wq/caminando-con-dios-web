00cf805e024f9ddb5e625ee373ac0c2665c522c4
caminandocondiosnicaragua-wq <caminandocondiosnicaragua@gmail.com>
2026-07-06 12:51:51 -0600

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,1 +1,39 @@
+/************************************************
+ * CAMINANDO CON DIOS
+ * CONFIGURACIÓN GLOBAL
+ ************************************************/
 
+const CONFIG = {
+
+    APP:{
+        nombre:"Caminando con Dios",
+        subtitulo:"Biblia y Fe",
+        version:"3.0"
+    },
+
+    TEMA:{
+        colorPrincipal:"#0D1B2A",
+        colorSecundario:"#1B365D",
+        colorDorado:"#D4AF37",
+        colorTexto:"#F5F7FA"
+    },
+
+    API:{
+        // Aquí irá la URL del Apps Script
+        sheets:"",
+        // API Bíblica
+        biblia:""
+
+    },
+    IDIOMA:{
+        defecto:"es",
+        automatico:true
+    },
+
+    DEVOCIONAL:{
+        bloquearDiasFuturos:true,
+        mostrarVideo:true,
+        mostrarComentarios:true,
+        mostrarLecturaBiblica:true
+    }
+};
