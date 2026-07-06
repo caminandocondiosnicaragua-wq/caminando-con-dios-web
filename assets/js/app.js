44d6f4726174a651766d95e54669ca5e669e6239
caminandocondiosnicaragua-wq <caminandocondiosnicaragua@gmail.com>
2026-07-06 12:52:49 -0600

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,39 +1,13 @@
-window.onload = iniciar;
+/************************************************
+ * CAMINANDO CON DIOS
+ * APP PRINCIPAL
+ ************************************************/
 
-async function iniciar() {
+async function iniciarAplicacion(){
 
-    try {
-
-        const datos = await obtenerDevocional();
-
-       // Obtener la fecha de hoy
-        const hoy = new Date();
-
-    // Formato YYYY-MM-DD
-        const año = hoy.getFullYear();
-        const mes = String(hoy.getMonth() + 1).padStart(2, "0");
-        const dia = String(hoy.getDate()).padStart(2, "0");
-
-        const fechaHoy = `${año}-${mes}-${dia}`;
-
-    // Buscar el devocional correspondiente a la fecha
-        const devocional = datos.find(item => item.FECHA === fechaHoy);
-
-    // Si no existe, mostrar el primero como respaldo
-        const devocionalFinal = devocional || datos[0];
-
-        mostrarDevocional(devocionalFinal);
-
-        document.getElementById("loader").style.display = "none";
-
-        document.getElementById("app").style.display = "block";
-
-    }
-
-    catch (error) {
-
-        console.error(error);
-
-    }
-
+    mostrarLoader();
+    await cargarDevocional();
+    ocultarLoader();
 }
+
+window.onload = iniciarAplicacion;
