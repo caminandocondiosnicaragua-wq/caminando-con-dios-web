ff8b1e1a0fc379ee8c83fd8b6a7a187319c5385e
caminandocondiosnicaragua-wq <caminandocondiosnicaragua@gmail.com>
2026-07-06 13:30:56 -0600

    Update navegacion.js

--- assets/js/interfaz/navegacion.js
+++ assets/js/interfaz/navegacion.js
@@ -1,1 +1,70 @@
+/************************************************
+ * CAMINANDO CON DIOS
+ * NAVEGACIÓN DEL DEVOCIONAL
+ ************************************************/
 
+
+/************************************************
+ * BOTONES
+ ************************************************/
+
+function crearAcciones(devocional){
+
+    return `
+
+<section class="acciones">
+
+    <button
+        class="btn secundario"
+        id="btnAnterior">
+
+        ← Día anterior
+
+    </button>
+
+    <button
+        class="btn principal"
+        id="btnCalendario">
+
+        📅 Elegir día
+
+    </button>
+
+    <button
+        class="btn secundario"
+        id="btnSiguiente">
+
+        Día siguiente →
+
+    </button>
+
+</section>
+
+<section class="acciones">
+
+    <button
+        class="btn lectura"
+        id="btnLectura">
+        📖 Comenzar lectura bíblica
+    </button>
+
+</section>
+`;
+}
+
+
+/************************************************
+ * ACTIVAR NAVEGACIÓN
+ ************************************************/
+
+function iniciarNavegacion(){
+
+    activarBotonAnterior();
+
+    activarBotonSiguiente();
+
+    activarCalendario();
+
+    activarModoLectura();
+
+}
