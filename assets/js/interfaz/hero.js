e3a651d5b4b2f4f04973d059344b08753b176216
caminandocondiosnicaragua-wq <caminandocondiosnicaragua@gmail.com>
2026-07-06 13:00:05 -0600

    Update hero.js

--- assets/js/interfaz/hero.js
+++ assets/js/interfaz/hero.js
@@ -1,1 +1,48 @@
+/************************************************
+ * CAMINANDO CON DIOS
+ * HERO
+ ************************************************/
 
+function crearHero() {
+
+    return `
+
+<section class="hero">
+    <img
+        src="assets/img/hero.png"
+        class="hero-imagen"
+        alt="Caminando con Dios">
+    <div class="hero-overlay">
+
+        <div class="hero-contenido">
+            <h1>
+                ${CONFIG.APP.nombre}
+            </h1>
+            <h2>
+                ${CONFIG.APP.subtitulo}
+            </h2>
+
+            <p class="hero-versiculo">
+                "Confía en Jehová con todo tu corazón,
+                y no te apoyes en tu propia prudencia."
+            </p>
+            <span>
+                Proverbios 3:5-6
+            </span>
+        </div>
+    </div>
+</section>
+
+`;
+
+}
+/************************************************
+ * CAMBIAR HERO
+ ************************************************/
+
+function cambiarHero(imagen){
+    const hero = document.querySelector(".hero-imagen");
+    if(hero){
+        hero.src = imagen;
+    }
+}
