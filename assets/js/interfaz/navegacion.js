10b1a3cf20320168cc3a8329bd2841708945c40e
caminandocondiosnicaragua-wq <caminandocondiosnicaragua@gmail.com>
2026-07-06 13:33:03 -0600

    Update navegacion.js

--- assets/js/interfaz/navegacion.js
+++ assets/js/interfaz/navegacion.js
@@ -68,3 +68,47 @@ function iniciarNavegacion(){
     activarModoLectura();
 
 }
+/************************************************
+ * DÍA ANTERIOR
+ ************************************************/
+
+function activarBotonAnterior(){
+    const boton = document.getElementById("btnAnterior");
+    if(!boton) return;
+    boton.addEventListener("click",()=>{
+        console.log("Abrir día anterior");
+    });
+}
+/************************************************
+ * DÍA SIGUIENTE
+ ************************************************/
+
+function activarBotonSiguiente(){
+    const boton = document.getElementById("btnSiguiente");
+    if(!boton) return;
+    boton.addEventListener("click",()=>{
+        console.log("Abrir día siguiente");
+    });
+}
+/************************************************
+ * CALENDARIO
+ ************************************************/
+
+function activarCalendario(){
+    const boton = document.getElementById("btnCalendario");
+    if(!boton) return;
+    boton.addEventListener("click",()=>{
+        console.log("Abrir calendario");
+    });
+}
+/************************************************
+ * MODO LECTURA
+ ************************************************/
+
+function activarModoLectura(){
+    const boton = document.getElementById("btnLectura");
+    if(!boton) return;
+    boton.addEventListener("click",()=>{
+        abrirModoLectura();
+    });
+}
