2ce9b32eaa1bb8719fb084592e4d75ac5f491891
caminandocondiosnicaragua-wq <caminandocondiosnicaragua@gmail.com>
2026-07-08 18:18:39 -0600

    Update hero.js

--- assets/js/interfaz/hero.js
+++ assets/js/interfaz/hero.js
@@ -3,46 +3,35 @@
  * HERO
  ************************************************/
 
-function crearHero() {
-
+function crearHero(){
     return `
-
 <section class="hero">
     <img
         src="assets/img/hero.png"
         class="hero-imagen"
-        alt="${CONFIG.APP.nombre}"
+        alt="${CONFIG.APP.nombre}">
     <div class="hero-overlay">
-
         <div class="hero-contenido">
-            <h1>
-                ${CONFIG.APP.nombre}
-            </h1>
             <h2>
-                ${CONFIG.APP.subtitulo}
+                ${CONFIG.APP.nombre}
             </h2>
-
-            <p class="hero-versiculo">
-    "${CONFIG.APP.versiculo}"
-</p>
-<span class="hero-cita">
-    ${CONFIG.APP.cita}
-</span>
-
+            <p>
+                ${CONFIG.APP.subtitulo}
+            </p>
         </div>
     </div>
 </section>
-
 `;
-
 }
+
+
 /************************************************
  * CAMBIAR HERO
  ************************************************/
 
 function cambiarHero(imagen){
-    const hero = document.querySelector(".hero-imagen");
+    const hero=document.querySelector(".hero-imagen");
     if(hero){
-        hero.src = imagen;
+        hero.src=imagen;
     }
 }
