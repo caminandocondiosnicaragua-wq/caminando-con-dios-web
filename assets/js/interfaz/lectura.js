0e11f8cde1b5640b554d3e0f964f2f4a740b0ce8
caminandocondiosnicaragua-wq <caminandocondiosnicaragua@gmail.com>
2026-07-23 17:45:37 -0600

    Update lectura.js
    AGREGUE A LA LINEA 177

--- assets/js/interfaz/lectura.js
+++ assets/js/interfaz/lectura.js
@@ -4,170 +4,105 @@
  ************************************************/
 
 let lecturaActiva = false;
-
 let scrollAnterior = 0;
 
 /************************************************
  * ESTADO DEL LECTOR BÍBLICO
  ************************************************/
-
 let lecturaActual = {
-
     lecturas: [],
-
     indiceLectura: 0,
-
     libro: "",
-
     capitulos: [],
-
     indiceCapitulo: 0
-
 };
 /************************************************
  * ABRIR MODO LECTURA
  ************************************************/
 
 function abrirModoLectura(){
-
     // Evitar abrir varias veces el lector
     if(document.getElementById("modoLectura")){
         return;
     }
-
     scrollAnterior = window.scrollY;
-
     lecturaActiva = true;
-
     // Obtener todas las lecturas del día
     lecturaActual.lecturas = obtenerCapitulosDelDia(devocionalActual);
-
     lecturaActual.indiceLectura = 0;
-
     const devocional = document.querySelector(".devocional");
-
     if(!devocional) return;
-
     devocional.style.display="none";
-
     mostrarPantallaLectura();
-
 }
 /************************************************
  * PANTALLA DE LECTURA
  ************************************************/
 
 function mostrarPantallaLectura(){
-   
-    if(document.getElementById("modoLectura")){
+     if(document.getElementById("modoLectura")){
     return;
 }
     const app = document.getElementById("app");
-
     const pantalla = document.createElement("section");
-
     pantalla.id="modoLectura";
-
     pantalla.className="modo-lectura";
-
     pantalla.innerHTML=`
-
         <div class="lectura-contenedor">
-
             <button
                 id="btnVolverDevocional"
                 class="btn secundario">
-
                 ← Volver al Devocional
-
             </button>
-
             <h2>
-
                 📖 Lectura Bíblica
-
             </h2>
-
             <p>
-
                 Antes de continuar con el devocional,
                 dedica unos minutos a leer la Palabra de Dios.
-
             </p>
-
             <br>
-
             <p>
-
                 Seleccione el capítulo que desea leer.
-
             </p>
-
             <div id="listaCapitulos">
-
             </div>
-
             <div id="textoBiblico">
-
             </div>
-
         </div>
-
     `;
-
   app.appendChild(pantalla);
-
 // Mostrar los capítulos del día
 mostrarCapitulos(lecturaActual.lecturas);
 
 iniciarModoLectura();
-
 }
 /************************************************
  * EVENTOS
  ************************************************/
-
 function iniciarModoLectura(){
-
     const boton=document.getElementById("btnVolverDevocional");
-
     if(!boton) return;
-
     boton.addEventListener("click",cerrarModoLectura);
-
 }
 /************************************************
  * CERRAR MODO LECTURA
  ************************************************/
 
 function cerrarModoLectura(){
-
     lecturaActiva=false;
-
     const pantalla=document.getElementById("modoLectura");
-
     if(pantalla){
-
         pantalla.remove();
-
     }
-
     const devocional=document.querySelector(".devocional");
-
     if(devocional){
-
         devocional.style.display="block";
-
     }
-
     window.scrollTo({
-
         top:scrollAnterior,
-
         behavior:"smooth"
-
     });
-
 }
 /************************************************
  * LISTA DE CAPÍTULOS
@@ -239,6 +174,7 @@ function crearTarjetas(idContenedor, lista){
                 item.capitulo
             );
             const html = crearHTMLCapitulo(datos);
+            console.log(html);
             mostrarTextoBiblico(html);
             }
             catch(error){
