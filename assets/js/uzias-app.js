7a718ba08d2404ead4eafaf53255b0b4fc783026
caminandocondiosnicaragua-wq <caminandocondiosnicaragua@gmail.com>
2026-08-08 16:31:01 -0600

    Add initial implementation of uzias-app.js

---
+++ assets/js/uzias-app.js
@@ -0,0 +1,44 @@
+/************************************************
+ * CAMINANDO CON DIOS
+ * ESTUDIO DEL REY UZÍAS
+ ************************************************/
+
+document.addEventListener("DOMContentLoaded", iniciarUzias);
+
+
+/************************************************
+ * INICIAR UZÍAS
+ ************************************************/
+
+function iniciarUzias(){
+
+    const app = document.getElementById("app");
+
+    app.innerHTML = `
+
+        ${crearHeader()}
+
+        <main class="estudio-uzias">
+
+            <div class="canva-estudio">
+
+                <iframe
+                    loading="lazy"
+                    src="https://www.canva.com/design/DAHRhjZ0oDM/I7KURDp9MuKK0IiskSHtVA/view?embed"
+                    allowfullscreen>
+                </iframe>
+
+            </div>
+
+        </main>
+
+        ${crearFooter()}
+
+    `;
+
+    app.style.display = "block";
+
+    iniciarHeader();
+    iniciarFooter();
+
+}
