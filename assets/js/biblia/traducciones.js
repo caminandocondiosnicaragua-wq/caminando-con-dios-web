3dacbb123b453ba31c69aea7467eae7318ef9903
caminandocondiosnicaragua-wq <caminandocondiosnicaragua@gmail.com>
2026-09-02 00:54:26 -0600

    Corregir idioma de New Living Bible

--- assets/js/biblia/traducciones.js
+++ assets/js/biblia/traducciones.js
@@ -8,7 +8,7 @@ const BIBLIAS_PAGINA = [
     {id:"01c25b8715dbb632-01",idioma:"es",idiomaNombre:"Español",nombre:"Nueva Versión Internacional",anio:"2025",titular:"Biblica, Inc."},
     {id:"2f416c6f03dd8090-02",idioma:"es",idiomaNombre:"Español",nombre:"Nueva Versión Internacional (Castilian)",anio:"",titular:"Biblica, Inc."},
     {id:"abc55ec4a92a291d-01",idioma:"es",idiomaNombre:"Español",nombre:"Nueva Versión Internacional",anio:"2015",titular:"Biblica, Inc."},
-    {id:"8676091b758f0d8d-01",idioma:"ja",idiomaNombre:"日本語 · Japonés",nombre:"Japanese Contemporary Bible",anio:"",titular:"Biblica, Inc."},
+    {id:"8676091b758f0d8d8-01",idioma:"ja",idiomaNombre:"日本語 · Japonés",nombre:"Japanese Contemporary Bible",anio:"",titular:"Biblica, Inc."},
     {id:"e959e47176271f18-01",idioma:"ko",idiomaNombre:"한국어 · Coreano",nombre:"Korean Living Bible",anio:"1985",titular:"Biblica, Inc."},
     {id:"35b94e98b2e3a01a-01",idioma:"pt",idiomaNombre:"Português · Portugués",nombre:"Nova Versão Internacional",anio:"",titular:"Biblica, Inc."},
     {id:"a47cbe7792801aa8-01",idioma:"pt",idiomaNombre:"Português · Portugués",nombre:"Nova Versão Internacional",anio:"2024",titular:"Biblica, Inc."},
@@ -34,7 +34,7 @@ const BIBLIAS_PAGINA = [
     {id:"74a986a9a183ae1c-01",idioma:"sl",idiomaNombre:"Slovenščina · Esloveno",nombre:"Slovenian Living New Testament",anio:"",titular:"Biblica, Inc.",alcance:"Nuevo Testamento"},
     {id:"7f996672a9b6d67a-01",idioma:"qu",idiomaNombre:"Quichua",nombre:"Quichua-Chimborazo New Testament",anio:"2016",titular:"Biblica, Inc.",alcance:"Nuevo Testamento"},
     {id:"c6229c708b3d1407-01",idioma:"",idiomaNombre:"Bette",nombre:"Bette Contemporary Bible",anio:"2025",titular:"Biblica, Inc."},
-    {id:"e1592fe262f26754-01",idioma:"en",idiomaNombre:"English · Inglés",nombre:"New Living Bible",anio:"",titular:"Biblica, Inc."},
+    {id:"e1592fe262f26754-01",idioma:"es",idiomaNombre:"Español",nombre:"New Living Bible",anio:"",titular:"Biblica, Inc."},
     {id:"cf5534eebe2d744f-01",idioma:"",idiomaNombre:"Sumi",nombre:"Sumi New Generation Bible",anio:"",titular:"Biblica, Inc."}
 ];
 
